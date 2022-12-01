7b47227940c2ff4fd3b1698ae994204ad11305f5
Mersedeh Sadeghi <mersedeh.sadeghi@mail.polimi.it>
2022-12-01 20:05:29 +0100

    1 dec

--- HW9/src/main/Calendar.java
+++ HW9/src/main/Calendar.java
@@ -9,6 +9,7 @@ public class Calendar {
 	}
 
 	public boolean isLeapYear() {
-		return (this.year > 0 && this.year % 4 == 0) && (this.year % 100 != 0 || this.year % 400 == 0);
+	  return (this.year > 1900 && this.year <=2000 && this.year % 4 == 0) && (this.year % 100 != 0 || this.year % 400 == 0);
+
 	}
 }
